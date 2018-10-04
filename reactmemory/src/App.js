8be214a21ef54220a6b3ac9e0359d875c4a1df98
Kevin Gonzalez <kevingonzalez@Kevins-MacBook-Pro.local>
2018-10-03 20:01:35 -0400

    final commit

--- reactmemory/src/App.js
+++ reactmemory/src/App.js
@@ -49,15 +49,12 @@ class App extends Component {
     }
   }
 
-  // I copy and pasted this randomize array function from 'Fisher-Yates Shuffle'
   reArrangeCards = (array) => {
     let currentIndex = array.length;
 
     while (0 !== currentIndex) {
-      // Pick a remaining element...
       let randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-      // And swap it with the current element.
       let temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
